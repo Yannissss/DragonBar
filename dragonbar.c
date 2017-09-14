56f0ba1a88bba390c52f96f0d29bfa4508dc7bfe
Yannissss <yannis.ondars@gmail.com>
2017-09-14 21:04:22 +0200

    Fixing

--- dragonbar.c
+++ dragonbar.c
@@ -179,7 +179,7 @@ int main(int argc, char const *argv[])
 			strcpy(song, meta);
 		
 		// Render
-		printf(",[{\"full_text\":\"%s   %3.1f Ko/s   %s   %s  %s  %s \"}] \n", \ 
+		printf(",[{\"full_text\":\"%s   %3.1f Ko/s   %s   %s  %s  %s \"}] \n",
 		song, bandwidth, volume, freespace, date, clock);
 
 		// Flush output stream
